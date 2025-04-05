70857f9647a883bb72158e09593a559a4dc772f7
chandra sekhar <119343229+ChandraSekhar002@users.noreply.github.com>
2025-04-05 22:24:52 +0530

    updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, ECS Fargate!! Your Node.js app is running.');
+    res.send('Hello, ECS Fargate!!! Your Node.js app is running.');
 });
 
 app.listen(port, () => {
